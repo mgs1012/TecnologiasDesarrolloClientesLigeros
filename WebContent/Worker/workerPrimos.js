155b4a90b0bd96376b1a8e0676a162a9827beedf
bl0823 <bl0823@192.168.7.20>
2014-10-28 13:48:04 +0100

    Fin clase mañana. Hacer ejercicio worker...

---
+++ WebContent/Worker/workerPrimos.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+var limInf = 1000000;
+var limSup = 1001000;
+
+function esPrimo(num){
+	var p = true;
+	for(var i=2;i<num;i++){
+		if(num%i == 0)
+			p = false;
+	}
+	return p;
+}
+  
+function calcularNumPrimos(){
+	if(){
+		
+	}
+}
